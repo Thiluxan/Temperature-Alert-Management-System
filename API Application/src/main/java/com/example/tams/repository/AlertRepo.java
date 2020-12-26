933048d024d49d5ea2c10f1a6872c882bf19bcec
Sivasubramaniam Elankumaran <48753796+Elankumaran98@users.noreply.github.com>
2020-12-26 21:59:34 +0530

    create AlertRepo into repository package

---
+++ API Application/src/main/java/com/example/tams/repository/AlertRepo.java
@@ -0,0 +1,8 @@
+package com.example.tams.repository;
+
+import com.example.tams.modal.Alert;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlertRepo extends JpaRepository<Alert,Integer> {
+}
+

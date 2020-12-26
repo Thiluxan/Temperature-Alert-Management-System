0676817ad2b2a13daf8771db1c42a20ef2b30815
Thiluxan Subramaniam <thiluxan@hotmail.co.uk>
2020-12-26 22:02:36 +0530

    Delete AlertRepo.java

--- API Application/src/main/java/com/example/tams/modal/AlertRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.tams.repository;
-
-import com.example.tams.modal.Alert;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlertRepo extends JpaRepository<Alert,Integer> {
-}
-
